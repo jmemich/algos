bb74434c89ba7372a14020975fd1ce26b8595aca
jmemich <jamesgmichelson@gmail.com>
2017-06-15 00:18:18 -0500

    REF make Node private

--- dequeue.py
+++ dequeue.py
@@ -1,10 +1,10 @@
 class LinkedDequeue:
 
-    class Node:
+    class _Node:
 
-        def __init__(self, item, next=None):
-            self.item = item
-            self.next = next
+        def __init__(self, _item, _next=None):
+            self._item = _item
+            self._next = _next
 
     def __init__(self):
         self.first = None
@@ -15,15 +15,15 @@ class LinkedDequeue:
 
     def enqueue(self, item):
         old_last = self.last
-        self.last = self.Node(item)
+        self.last = self._Node(item)
         if self.is_empty():
             self.first = self.last
         else:
-            old_last.next = self.last
+            old_last._next = self.last
     
     def dequeue(self):
-        item = self.first.item
-        self.first = self.first.next
+        item = self.first._item
+        self.first = self.first._next
         if self.is_empty():
             self.last = None
         return item
