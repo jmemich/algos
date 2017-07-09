f2eadbf351197a47e2b11d399a63bbcd60afdb6d
jmemich <jamesgmichelson@gmail.com>
2017-07-08 22:17:30 -0500

    REF simplify & early stopping

--- linreg.cpp
+++ linreg.cpp
@@ -4,63 +4,39 @@
 
 using namespace Eigen;
 
-struct GradParams
-{
-    float b0;
-    Eigen::VectorXf betas;
-};
-
-// TODO vectorize
-GradParams gradient(const Eigen::VectorXf & x,
-                    const float & y,
-                    const Eigen::VectorXf & betas,
-                    const float & b0)
-{
-    GradParams gparams;
-    float y_hat = b0+betas.dot(x);
-    gparams.b0 = -2*(y-y_hat); // dB0/dB0 = 1
-    gparams.betas = -2*(y-y_hat)*x; // dBiXi/dBi = Xi
-    return gparams;
-}
-
-float loss(const Eigen::MatrixXf & X,
-           const Eigen::VectorXf & y,
-           const Eigen::VectorXf & betas,
-           const float b0)
-{
-    float err = 0;
-    float y_hat;
-    for (int i=0; i<X.rows(); i++) {
-        y_hat = b0 + betas.dot(X.row(i));
-        err += std::pow(y[i]-y_hat, 2);
-    }
-    return err/X.rows();
-}
-
-void linreg(const Eigen::MatrixXf & X,
-            const Eigen::VectorXf & y) {
-    // TODO set seed
-    float b0 = 1.0; // TODO make random
-    Eigen::VectorXf betas = Eigen::VectorXf::Ones(3); // TODO make random
-
-    float err;
-    int steps = 10000;
-    float alpha = 0.01;
+VectorXf linreg(const MatrixXf & X,
+                const VectorXf & y,
+                const float & alpha,
+                const int & steps) {
+    float b0 = 0.0;
+    VectorXf betas = VectorXf::Random(3);  // TODO seed
+    float err, y_hat;
     for (int step=0; step<steps; step++) { 
+        err = 0;
         for (int i=0; i<X.rows(); i++) {
-            GradParams gparams = gradient(X.row(i), y[i], betas, b0);
-            b0 -= alpha*gparams.b0;
-            betas -= alpha*gparams.betas;
+            y_hat = b0+betas.dot(X.row(i));
+            b0 -= alpha*-2*(y[i]-y_hat); // dB0/dB0 = 1
+            betas -= alpha*-2*(y[i]-y_hat)*X.row(i); // dBiXi/dBi = Xi
+            err += std::pow(y[i]-y_hat,2);
         }
-        err = loss(X,y,betas,b0);
-        // std::cout << "step: " << step+1 << "|err: " << err << std::endl;;
+        err /= X.rows();
+        if (err < 1e-10) {  // early stopping
+            std::cout << "stopping early on step " << step+1
+                      << " with error " << err
+                      << std::endl;
+            break;
+        }
     }
-    std::cout << "b0: " << b0 << std::endl;
-    std::cout << "betas: " << betas.transpose() << std::endl;
+    VectorXf result = VectorXf(1+betas.rows());
+    result(0) = b0;
+    for (int i=0; i<betas.rows(); i++) {
+        result(i+1) = betas(i);
+    }
+    return result;
 }
 
 int main() {
-    Eigen::MatrixXf X(10,3);
+    MatrixXf X(10,3);
     X << 1.1,1,10,
          2.1,1,1,
          3.1,1,9,
@@ -72,8 +48,12 @@ int main() {
          9.1,2,6,
          10.1,2,5;
 
-    Eigen::VectorXf y(10);
+    VectorXf y(10);
     y << 1,2,3,4,5,6,7,8,9,10;
 
-    linreg(X,y);
+    float alpha = 0.01;
+    int steps = 10000;
+    VectorXf params = linreg(X,y,alpha,steps);
+    std::cout << "parameters: " << params.transpose() << std::endl;
+    return 0;
 }
